ec19d48c7e961eeac47d5af3206a763c3732f4fb
msunh <msalto.unh@gmail.com>
2021-06-22 23:46:52 -0300

    terminando analizador estadisticas

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/Analizador.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/Analizador.kt
@@ -1,35 +1,71 @@
 package ar.edu.unahur.obj2.servidorWeb
 
+import java.time.LocalDateTime
+
 interface Analizador{
 
     fun analizar(respuesta:Respuesta,modulo:Modulo)
+
 }
 
 class DeteccionDeDemoraEnRespuesta(val demoraMinima: Int) : Analizador {
-    var respuestasDemoradas = 0
 
 
     override fun analizar(respuesta: Respuesta,modulo: Modulo) {
         if (respuesta.tiempo > demoraMinima){
-            respuestasDemoradas += 1
+            modulo.respuestasDemoradas.add(respuesta)
         }
     }
 
-    fun cantidadDeRespuestasDemoradas() = respuestasDemoradas
+    fun cantidadDeRespuestasDemoradas(modulo : Modulo) = modulo.respuestasDemoradas.size
 }
 
-class IpsSospechosas(val listaDeIps : List<String>) : Analizador{
+class IpsSospechosas(val listaDeIps: List<String>) : Analizador{
+    val listaDeIpsSospechosasAnalizadas = mutableListOf<Respuesta>()
+    val modulosConsultadosPorIpSospechosas = mutableListOf<Modulo>()
 
     override fun analizar(respuesta: Respuesta,modulo: Modulo){
-        TODO()
+        if (listaDeIps.contains(respuesta.pedido.ip)) {
+            listaDeIpsSospechosasAnalizadas.add(respuesta)
+            modulo.sumarConsultaSospechosa()
+            modulosConsultadosPorIpSospechosas.add(modulo)
+        }
+    }
+
+    fun cuantosPedidosRealizo(ip: String) =
+        listaDeIpsSospechosasAnalizadas.count{it.pedido.ip == ip}
+
+
+    fun moduloMasConsultadoPorIpsSospechosas() : Modulo{
+        return modulosConsultadosPorIpSospechosas.maxByOrNull{it.cantConsultasSospechosas}!!
+    }
+
+    fun conjuntoDeIpsSospechosas(ruta : String) : Set<String>{
+        val listaDePedidos = listaDeIpsSospechosasAnalizadas.map{it.pedido}
+        return listaDePedidos.filter{it.ruta() == ruta}.map{it.ip}.toSet()
     }
 
 }
 
 class Estadisticas() : Analizador{
+    val listaDeRespuestas = mutableListOf<Respuesta>()
 
     override fun analizar(respuesta: Respuesta, modulo: Modulo) {
-        TODO("Not yet implemented")
+        listaDeRespuestas.add(respuesta)
     }
+
+    fun tiempoDeRespuestaPromedio() =
+        listaDeRespuestas.sumBy{it.tiempo} / listaDeRespuestas.size
+
+
+    fun cantidadDePedidosEntreDosMomentos(fecha1 : LocalDateTime, fecha2:LocalDateTime):Int {
+       return listaDeRespuestas.count { it.pedido.fechaHora in fecha1..fecha2 }
+    }
+
+    fun respuestasConBodyTipo(stringDeterminado : String) : Int {
+
+        return listaDeRespuestas.count{it.body.contains(stringDeterminado) }
+    }
+
 }
 
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -1,10 +1,10 @@
 package ar.edu.unahur.obj2.servidorWeb
 
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.collections.shouldContain
-import io.kotest.matchers.should
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
 import java.time.LocalDateTime
+import java.time.Month
 
 class ServidorWebTest : DescribeSpec({
   describe("Un servidor web") {
@@ -69,8 +69,8 @@ class ServidorWebTest : DescribeSpec({
     val otroPedidoConTexto = Pedido("192.168.1.10", "http://juampi.com.ar/virus.doc", LocalDateTime.now())
     val unPedidoDeVideo = Pedido("192.168.1.103", "http://bardo.com.ar/keloke.avi", LocalDateTime.now())
     val moduloDeImagen = Modulo(listOf("png","jpg","gif","raw"), "soy un modulo de imagen", 50)
-    val moduloDeTexto = Modulo(listOf("txt","doc", "odt"), "soy un modulo de binarios", 9)
-    val moduloDeVideo = Modulo(listOf("avi","3gp","mkv","mpeg"), "soy un modulo de video", 12)
+    val moduloDeTexto = Modulo(listOf("txt","doc", "odt"), "soy un modulo de texto", 9)
+    val moduloDeVideo = Modulo(listOf("avi","3gp","mkv","mpeg"), "soy un modulo de video", 20)
     val analizadorDeDemora = DeteccionDeDemoraEnRespuesta(10)
 
     servidorWeb.agregarModulo(moduloDeTexto)
@@ -85,9 +85,115 @@ class ServidorWebTest : DescribeSpec({
 
     it("probando analizador deteccion de demora en respuesta"){
 
-      analizadorDeDemora.cantidadDeRespuestasDemoradas().shouldBe(2)
+      analizadorDeDemora.cantidadDeRespuestasDemoradas(moduloDeImagen).shouldBe(1)
 
     }
+
+    it ("probando analizador deteccion de demora en respuesta , con modulo no demorado"){
+      analizadorDeDemora.cantidadDeRespuestasDemoradas(moduloDeTexto).shouldBe(0)
+    }
+
+  }
+
+  describe ("analizadores - Ips sospechosas"){
+    val servidorWeb = ServidorWeb()
+
+    val unPedidoConImagen = Pedido("192.168.1.105", "http://www.pipo.com.ar/documentos/imagen.jpg",LocalDateTime.now())
+    val otroPedidoConImagen = Pedido("192.168.1.102", "http://www.jorgito.com.ar/documentos/imagen.jpg",LocalDateTime.now())
+    val tercerPedidoConImagen = Pedido("192.168.1.102", "http://www.juancito.com.ar/documentos/imagen.jpg",LocalDateTime.now())
+
+    val otroPedidoConTexto = Pedido("192.168.1.150", "http://juampi.com.ar/virus.doc", LocalDateTime.now())
+
+
+    val unPedidoDeVideo = Pedido("192.168.1.104", "http://bardo.com.ar/keloke.avi", LocalDateTime.now())
+    val otroPedidoDeVideo = Pedido("192.168.1.104", "http://iutub.com.ar/avion.avi", LocalDateTime.now())
+    val tercerPedidoDeVideo = Pedido("192.168.1.104", "http://github.com.ar/tutorial.avi", LocalDateTime.now())
+    val cuartoPedidoDeVideo = Pedido("192.168.1.104", "http://qwerty.com.ar/tutorial.avi", LocalDateTime.now())
+
+    val moduloDeImagen = Modulo(listOf("png","jpg","gif","raw"), "soy un modulo de imagen", 50)
+    val moduloDeTexto = Modulo(listOf("txt","doc", "odt"), "soy un modulo de texto", 9)
+    val moduloDeVideo = Modulo(listOf("avi","3gp","mkv","mpeg"), "soy un modulo de video", 20)
+    val analizadorDeIpsSospechosas = IpsSospechosas(listOf("192.168.1.102","192.168.1.104","192.168.1.105" ))
+
+    servidorWeb.agregarModulo(moduloDeTexto)
+    servidorWeb.agregarModulo(moduloDeVideo)
+    servidorWeb.agregarModulo(moduloDeImagen)
+    servidorWeb.agregarAnalizador(analizadorDeIpsSospechosas)
+
+    servidorWeb.atenderPedido(unPedidoConImagen)
+    servidorWeb.atenderPedido(otroPedidoConImagen)
+    servidorWeb.atenderPedido(tercerPedidoConImagen)
+    servidorWeb.atenderPedido(otroPedidoConTexto)
+    servidorWeb.atenderPedido(unPedidoDeVideo)
+    servidorWeb.atenderPedido(otroPedidoDeVideo)
+    servidorWeb.atenderPedido(tercerPedidoDeVideo)
+    servidorWeb.atenderPedido(cuartoPedidoDeVideo)
+
+    it("cuantos pedidos realizo una ip sospechosa"){
+      analizadorDeIpsSospechosas.cuantosPedidosRealizo("192.168.1.104").shouldBe(4)
+    }
+
+    it("modulo más consultado por ips sospechosas"){
+      analizadorDeIpsSospechosas.moduloMasConsultadoPorIpsSospechosas().shouldBe(moduloDeVideo)
+    }
+
+    it("conjunto de ips sospechosas que requirieron una cierta ruta"){
+      analizadorDeIpsSospechosas.conjuntoDeIpsSospechosas("/documentos/imagen.jpg").shouldContainExactlyInAnyOrder(listOf("192.168.1.102","192.168.1.105" ))
+    }
+
+  }
+
+  describe ("estadisticas"){
+    val servidorWeb = ServidorWeb()
+
+    val unPedidoConImagen = Pedido("192.168.1.105", "http://www.pipo.com.ar/documentos/imagen.jpg",LocalDateTime.of(2021,Month.JUNE,5,14,14))
+    val otroPedidoConImagen = Pedido("192.168.1.102", "http://www.jorgito.com.ar/documentos/imagen.jpg",LocalDateTime.of(2021,Month.JUNE,8,14,14))
+    val tercerPedidoConImagen = Pedido("192.168.1.102", "http://www.juancito.com.ar/documentos/imagen.jpg",LocalDateTime.of(2021,Month.JUNE,15,14,14))
+
+    val otroPedidoConTexto = Pedido("192.168.1.150", "http://juampi.com.ar/virus.doc", LocalDateTime.of(2021, Month.JUNE,21,20,12))
+
+
+    val unPedidoDeVideo = Pedido("192.168.1.104", "http://bardo.com.ar/keloke.avi", LocalDateTime.of(2021, Month.JUNE,2,11,12))
+    val otroPedidoDeVideo = Pedido("192.168.1.104", "http://iutub.com.ar/avion.avi", LocalDateTime.of(2021, Month.JUNE,19,12,12))
+    val tercerPedidoDeVideo = Pedido("192.168.1.104", "http://github.com.ar/tutorial.avi", LocalDateTime.of(2021, Month.JUNE,17,23,12))
+    val cuartoPedidoDeVideo = Pedido("192.168.1.104", "http://qwerty.com.ar/tutorial.avi", LocalDateTime.of(2021, Month.JUNE,20,2,1))
+    val moduloDeImagen = Modulo(listOf("png","jpg","gif","raw"), "soy un modulo de imagen, hola! ", 50)
+    val moduloDeTexto = Modulo(listOf("txt","doc", "odt"), "soy un modulo de texto, saludos", 8)
+    val moduloDeVideo = Modulo(listOf("avi","3gp","mkv","mpeg"), "soy un modulo de video, ! saludos?", 20)
+    val analizadorDeEstadisticas = Estadisticas()
+
+    servidorWeb.agregarModulo(moduloDeTexto)
+    servidorWeb.agregarModulo(moduloDeVideo)
+    servidorWeb.agregarModulo(moduloDeImagen)
+    servidorWeb.agregarAnalizador(analizadorDeEstadisticas)
+
+    servidorWeb.atenderPedido(unPedidoConImagen)
+    servidorWeb.atenderPedido(otroPedidoConImagen)
+    servidorWeb.atenderPedido(tercerPedidoConImagen)
+    servidorWeb.atenderPedido(otroPedidoConTexto)
+    servidorWeb.atenderPedido(unPedidoDeVideo)
+    servidorWeb.atenderPedido(otroPedidoDeVideo)
+    servidorWeb.atenderPedido(tercerPedidoDeVideo)
+    servidorWeb.atenderPedido(cuartoPedidoDeVideo)
+
+
+
+
+    it("tiempo de respuesta promedio"){
+      analizadorDeEstadisticas.tiempoDeRespuestaPromedio().shouldBe(29)
+    }
+
+    it("cantidad De Pedidos entre dos momentos -fecha/hora- que fueron atendidos"){
+      analizadorDeEstadisticas.cantidadDePedidosEntreDosMomentos(LocalDateTime.of(2021, Month.JUNE,10,23,12), LocalDateTime.now()).shouldBe(5)
+    }
+
+    it("cantidad de respuestas cuyo body incluye determinado string"){
+      analizadorDeEstadisticas.respuestasConBodyTipo("saludos").shouldBe(5)
+    }
+
+    
+
+
 
   }
 
